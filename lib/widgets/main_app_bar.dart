7c97308de14fa40aafda310dcd507b0b06a13c7c
minvs1 <achmedovas.tomas@gmail.com>
2020-05-31 00:50:02 +0300

    Move app bar to widgets

---
+++ lib/widgets/main_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(context) {
+    return AppBar(
+      centerTitle: true,
+      title: Text('Keeper'),
+      actions: [
+        IconButton(
+          splashColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          icon: Icon(Icons.storage),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
